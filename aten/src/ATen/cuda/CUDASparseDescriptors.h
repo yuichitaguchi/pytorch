e6731b7053a48e3329e7e5f24ba6ebb220da9b84
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2021-08-31 10:58:05 +0000

    Sparse CSR CUDA: add `torch.addmm` with all inputs sparse
    This PR adds `torch.addmm(c, a, b)` variant with `c, a, b` all being CSR tensors.
    The underlying cuSPARSE function works only with 32-bit indices, and in
    the current implementation the result tensor has 32-bit indices. Input
    tensors can have both 64-bit and 32-bit indices tensors.
    
    ghstack-source-id: 0078ae9a384df7b42c5a98eab56416a305e9c8d3
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/63511

--- aten/src/ATen/cuda/CUDASparseDescriptors.h
+++ aten/src/ATen/cuda/CUDASparseDescriptors.h
@@ -218,6 +218,33 @@ class TORCH_CUDA_CPP_API CuSparseSpMatCsrDescriptor
     descriptor_.reset(raw_descriptor);
   }
 
+#if defined(CUDA_VERSION) && CUDA_VERSION >= 11000
+  std::tuple<int64_t, int64_t, int64_t> get_size() {
+    int64_t rows, cols, nnz;
+    TORCH_CUDASPARSE_CHECK(cusparseSpMatGetSize(
+        this->descriptor(),
+        &rows,
+        &cols,
+        &nnz));
+    return std::make_tuple(rows, cols, nnz);
+  }
+
+  void set_tensor(const Tensor& input) {
+    auto crow_indices = input.crow_indices();
+    auto col_indices = input.col_indices();
+    auto values = input.values();
+
+    TORCH_INTERNAL_ASSERT_DEBUG_ONLY(crow_indices.is_contiguous());
+    TORCH_INTERNAL_ASSERT_DEBUG_ONLY(col_indices.is_contiguous());
+    TORCH_INTERNAL_ASSERT_DEBUG_ONLY(values.is_contiguous());
+    TORCH_CUDASPARSE_CHECK(cusparseCsrSetPointers(
+        this->descriptor(),
+        crow_indices.data_ptr(),
+        col_indices.data_ptr(),
+        values.data_ptr()));
+  }
+#endif
+
 #if AT_USE_CUSPARSE_GENERIC_SPSV()
   void set_mat_fill_mode(bool upper) {
     cusparseFillMode_t fill_mode =
@@ -260,6 +287,18 @@ class TORCH_CUDA_CPP_API CuSparseSpSMDescriptor
   CuSparseSpSMDescriptor() {
     cusparseSpSMDescr_t raw_descriptor;
     TORCH_CUDASPARSE_CHECK(cusparseSpSM_createDescr(&raw_descriptor));
+    descriptor_.reset(raw_descriptor);
+  }
+};
+#endif
+
+#if defined(CUDA_VERSION) && CUDA_VERSION >= 11000
+class TORCH_CUDA_CPP_API CuSparseSpGEMMDescriptor
+    : public CuSparseDescriptor<cusparseSpGEMMDescr, &cusparseSpGEMM_destroyDescr> {
+ public:
+  CuSparseSpGEMMDescriptor() {
+    cusparseSpGEMMDescr_t raw_descriptor;
+    TORCH_CUDASPARSE_CHECK(cusparseSpGEMM_createDescr(&raw_descriptor));
     descriptor_.reset(raw_descriptor);
   }
 };
