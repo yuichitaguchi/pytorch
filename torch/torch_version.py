f61f29687691393d715fbee739002bc7ca4d0b14
Eli Uriegas <eliuriegas@fb.com>
2021-09-09 11:37:17 -0700

    DO NOT MERGE, testing flake8 annotations
    Signed-off-by: Eli Uriegas <eliuriegasfb.com>
    
    ghstack-source-id: 15f92a50d2e52a24233bc9deed29170d9ec73ef0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/64450
    
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- torch/torch_version.py
+++ torch/torch_version.py
@@ -31,9 +31,9 @@ class TorchVersion(str):
             TorchVersion('1.10.0a') > '1.2'
             TorchVersion('1.10.0a') > '1.2.1'
     """
-    # fully qualified type names here to appease mypy
+    #fully qualified type names here to appease mypy
     def _convert_to_version(self, inp: Union[packaging.version.Version, str, Iterable]) -> packaging.version.Version:
-        if isinstance(inp, Version):
+        if isinstance(inp, Version):#whatup
             return inp
         elif isinstance(inp, str):
             return Version(inp)
