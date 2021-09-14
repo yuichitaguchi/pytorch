8ec57ec2d5e5d4405a29c89286a0384041229da8
mingfeima <mingfei.ma@intel.com>
2021-09-14 10:50:50 +0800

    change default grain size in gather scatter kernel to improve CPU performance
    ghstack-source-id: 3f665187dd634af3a28d7a5880dc8bfa702a9d07
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/64478

--- aten/src/ATen/native/cpu/ScatterGatherKernel.cpp
+++ aten/src/ATen/native/cpu/ScatterGatherKernel.cpp
@@ -131,6 +131,10 @@ struct cpu_scatter_gather_base_kernel {
 
     auto index_upper_bound = self_dim_size;
 
+    // since the index dimension is squashed, need to alter the grain size according
+    // to keep equal granularity in parallelism.
+    int64_t grain_size = std::max((int64_t) 1, at::internal::GRAIN_SIZE / index_dim_size);
+
     AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND3(
       ScalarType::Bool, ScalarType::Half, ScalarType::BFloat16, iter.dtype(),
       "scatter_gather_scalar_cpu", [&] {
@@ -142,10 +146,8 @@ struct cpu_scatter_gather_base_kernel {
           auto* index_data_bytes = data[INDEX_ITER_STRIDE_IDX];
           // we change the order of TensorIterator-dim loop
           // vs dim-TensorIterator loop order depending on
-          // whether dim is the last dimension and/or
-          // whether `n` is smaller than `index_dim_size`
-
-          if ((dim== self.dim() - 1) || (n < index_dim_size)) {
+          // whether dim is the last dimension
+          if (dim== self.dim() - 1) {
             for (int64_t nelem = 0; nelem < n; ++nelem) {
               // dim loop is a separate code block
               // for better performance
@@ -181,7 +183,7 @@ struct cpu_scatter_gather_base_kernel {
             }
           }
         };
-        iter.for_each(loop);
+        iter.for_each(loop, grain_size);
       }
     );
   }
@@ -212,6 +214,8 @@ struct cpu_scatter_gather_base_kernel {
 
     auto index_upper_bound = is_scatter_like ? self_dim_size : src_dim_size;
 
+    int64_t grain_size = std::max((int64_t) 1, at::internal::GRAIN_SIZE / index_dim_size);
+
     AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND3(
       ScalarType::Bool, ScalarType::Half, ScalarType::BFloat16, iter.dtype(),
       "scatter_gather_tensor_cpu", [&] {
@@ -224,9 +228,8 @@ struct cpu_scatter_gather_base_kernel {
           auto* src_data_bytes = data[SRC_ITER_STRIDE_IDX];
           // we change the order of TensorIterator-dim loop
           // vs dim-TensorIterator loop order depending on
-          // whether dim is the last dimension and/or
-          // whether `n` is smaller than `index_dim_size`
-          if ((dim== self.dim() - 1) || (n < index_dim_size)) {
+          // whether dim is the last dimension
+          if (dim== self.dim() - 1) {
             for (int64_t nelem = 0; nelem < n; ++nelem) {
               // dim loop is a separate code block
               // for better performance
@@ -268,7 +271,7 @@ struct cpu_scatter_gather_base_kernel {
             }
           }
         };
-        iter.for_each(loop);
+        iter.for_each(loop, grain_size);
       }
     );
   }
